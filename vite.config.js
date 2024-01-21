9e24a5537db1c30c749ebdcb91a2543cba857637
Eyuel Frew <eyuelfre430@gmail.com>
2024-01-21 18:45:21 +0300

    cantwork

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,13 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://chat-app-back-6bsl.onrender.com",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      },
-    },
-  },
+  // server: {
+  // proxy: {
+  //   "/api": {
+  //     target: "https://chat-app-back-6bsl.onrender.com",
+  //     changeOrigin: true,
+  //     rewrite: (path) => path.replace(/^\/api/, ""),
+  //   },
+  // },
+  // },
 });
